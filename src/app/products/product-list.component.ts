2bc33c96be14a1a20d2b3e07ad688d2aec20f945
Luly Atilano <maria.atilano@globant.com>
2020-04-25 21:00:31 -0500

    specified if the variables in ProductListComponent are public or private,  deleted the mock array for products;  added the service on the constructor, added the products, and filteredProducts inside of ngOnInit.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -8,11 +9,11 @@ import { IProduct } from './product';
 })
 
 export class ProductListComponent implements OnInit {
-  pageTitle = 'Product List';
-  imageWidth = 50;
-  imageMargin = 2;
-  showImage = false;
-  _listFilter: string;
+  public pageTitle = 'Product List';
+  public imageWidth = 50;
+  public imageMargin = 2;
+  public showImage = false;
+  public _listFilter: string;
 
   get listFilter(): string {
     return this._listFilter;
@@ -22,36 +23,16 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png'
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2019',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png'
-    },
-  ];
+  products: IProduct[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = '';
+  constructor(
+    private productService: ProductService
+  ) {
   }
 
   ngOnInit(): void {
-    console.log('in OnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   toggleImage(): void {
